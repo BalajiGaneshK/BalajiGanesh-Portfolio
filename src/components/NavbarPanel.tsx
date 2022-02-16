70473b570822ae9284bf72fe6b59a667c07fb77d
Balaji Ganesh <balaji@optanium.com>
2022-02-16 15:21:21 +0530

    initialised

---
+++ src/components/NavbarPanel.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "styles/NavbarPanel.css";
+
+const NavbarPanel = () => {
+  return (
+    <div className="navbarContainer">
+      <div className="navbarPanel">
+        <div className="logo">
+          <img src={"../../public/icons/apple-icon.png"} />
+        </div>
+        <div className="navMenu"></div>
+        <p>hi</p>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarPanel;
